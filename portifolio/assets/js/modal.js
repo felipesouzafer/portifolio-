4f17fc77a91e19d3b79bc26824758efed59bdead
felipppe2005 <113396587+felipppe2005@users.noreply.github.com>
2023-11-19 09:57:45 -0300

    alguns ajustes no pop up

--- portifolio/assets/js/modal.js
+++ portifolio/assets/js/modal.js
@@ -1,10 +1,10 @@
 function abrirModal(){
     const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
+   modal.classList.add('abrir')
 
-    modal.addEventListener('click', (e) =>{
+  modal.addEventListener('click', (e) =>{
         if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
             modal.classList.remove('abrir')
-        }
+       }
     })
 }
